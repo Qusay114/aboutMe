5e837ce174b359caf6898e14978912a967663306
Qusay114 <eng.qusay015@gmail.com>
2021-03-15 14:54:50 +0200

    upload the pages

---
+++ js/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let q1 = prompt('Is my name Ahmad').toLowerCase();
+if(q1==='yes' || q1==='y')
+{
+  alert('Noooo  ,That is not my name , my name is Qusay, it is a nice name right ? ^_^');
+  console.log('Noooo  ,That is not my name , my name is Qusay, it is a nice name right ? ^_^');
+}
+else
+{
+  alert('yeah that is correct , that is not my name , My name is Qusay , it is a nice name right ? ^_^');
+  console.log('yeah that is correct , that is not my name , My name is Qusay , it is a nice name right ? ^_^');
+}
+
+let q2 = prompt('Am I 40 old ?').toLowerCase();
+if(q2==='yes' || q2==='y')
+{
+  alert('Noooo  ,I\'m not that old come on -_-');
+  console.log('Noooo  ,I\'m not that old come on -_-');
+}
+else
+{
+  alert('you are right , I\'m not that old , I\'m 23 old , still young and looking for a girl to marry ,haha I am just joking I do not want to kill my self early :) ');
+  console.log('you are right , I\'m not that old , I\'m 23 old , still young and looking for a girl to marry ,haha I am just joking I do\'nt want to die early :) ');
+}
+
+let q3 = prompt('Am I an web developer ?').toLowerCase();
+if(q3==='yes' || q3==='y')
+{
+  alert('yeah that\'is right , and I hope to become one of the greatest programmers ');
+  console.log('yeah that\'is right , and I hope to become one of the greatest programmers ');
+}
+else
+{
+  alert('no , come on be realistic you like my work , who does not :) ');
+  console.log('no , come on be realistic you like my work , who does not :) ');
+}
+
+let q4 = prompt('Do I live in jordan ?').toLowerCase();
+if(q4==='yes' || q4==='y')
+{
+  alert('yeah that\'is right , fortunetly I live in this great country');
+  console.log('yeah that\'is right , fortunetly I live in this great country');
+}
+else
+{
+  alert('you are wrong , I live in jordan ');
+  console.log('you are wrong , I live in jordan ');
+}
+
+let q5 = prompt('Do you think I prefer online classes ? ').toLowerCase();
+if(q5==='yes' || q5==='y')
+{
+  alert('yes that\'s right I do prefer them because it saves me time and effort of transporting');
+  console.log('yes that\'s right I do prefer them because it saves me time and effort of transporting');
+}
+else
+{
+  alert('no that\'s wrong . I do prefer online classes');
+  console.log('no that\'s wrong . I do prefer online classes');
+}
+
+
+let name1 = prompt('What is your name by the way ?');
+
+alert('that is really a beautiful name, nice to meet you ' + name1 +' ^_^');
+
+
